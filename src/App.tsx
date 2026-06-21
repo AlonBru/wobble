3fb75b69d13369cba765a51e9463dc6db5f3cb46
AlonBru <indyalon@gmail.com>
2026-06-21 22:14:37 +0300

    improve

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import Snow from './assets/snojito_b.svg?react'
 import './App.css'
 
 function App() {
-
+  
   return (
     <>
         <div className="hero">
           <Snow />
+          <div className='h-c'>
+
+          <h1 style={{
+          }}>
+            Get well
+          </h1>
+          <h1 style={{
+            
+          }}>
+            so fucking soon
+          </h1>
+          </div>
         </div>
    
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,5 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     svgr()
   ],
+  base: '.'
 })
