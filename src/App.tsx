91b0744ba6357eca73609b366b9537bf99042e9d
AlonBru <indyalon@gmail.com>
2026-06-21 21:40:52 +0300

    Updates

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Snow from './assets/snojito_b.svg?react'
+// import MyIcon from './assets/icon.svg?react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="hero">
+          <Snow />
+        </div>
+   
+    </>
+  )
+}
+
+export default App
