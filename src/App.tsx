4d4a810d7b4f54478c61c9c3b9da5a673a2017a8
AlonBru <indyalon@gmail.com>
2026-06-21 23:37:39 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,29 @@ function App() {
           <div className='h-c'>
 
           <h1           >
-            <span className='n-1'>Dear Emma</span>
+            <span className='n-1'>Darling Emma,</span>
           </h1>
           <h1           >
             <span>Get well</span>
           </h1>
-          <h1>
+          <h1 style={{translate:'-10vw'}}>
             
-            <span className='n1'>so</span> <span className='n1'>fucking</span> <span className='n1'>soon</span>
+            <span className='n1'>ever so</span> 
+          </h1>
+          <h1 style={{translate:'-20vw'}}>
+            
+            <span className='n2'>fucking</span> 
+          </h1>
+            
+          <h1 style={{translate:'-18vw'}}>
+            <span className='n3'>soon</span>
           </h1>
           </div>
           <button 
             style={{
-              position:'absolute',bottom:0,right:0
+              position:'absolute',bottom:10,right:10
             }}
-          onClick={()=>location.reload()}>🔄 Refresh</button>
+          onClick={()=>location.reload()}>GO AGAIN</button>
         </div>
 
    
