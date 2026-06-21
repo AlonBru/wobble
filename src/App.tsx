7efe26cb25bc4c10194ff3c55bb40800fb3f6233
AlonBru <indyalon@gmail.com>
2026-06-21 22:58:45 +0300

    dear

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,24 @@ function App() {
           <Snow />
           <div className='h-c'>
 
-          <h1 style={{
-          }}>
+          <h1           >
+            <span className='n-1'>Dear Emma</span>
+          </h1>
+          <h1           >
             <span>Get well</span>
           </h1>
-          <h1 style={{
-            
-          }}>
+          <h1>
             
             <span className='n1'>so</span> <span className='n1'>fucking</span> <span className='n1'>soon</span>
           </h1>
           </div>
+          <button 
+            style={{
+              position:'absolute',bottom:0,right:0
+            }}
+          onClick={()=>location.reload()}>🔄 Refresh</button>
         </div>
+
    
     </>
   )
